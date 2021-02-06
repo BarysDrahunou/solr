4b295c1ff182d2c2ae3f665f161922c335115bc9
Barys Drahunou <barys_drahunou@epam.com>
2021-02-06 10:32:23 +0300

    finish searching by fields

--- searching_module/src/main/java/solr/SearchController.java
+++ searching_module/src/main/java/solr/SearchController.java
@@ -15,7 +15,12 @@ import java.util.stream.Collectors;
 @Controller
 public class SearchController {
 
-    private Map<String, Book> bookMap;
+    private final int fullMatchedBooksPriority = 10;
+    private final int limitForWordsInQuery = 3;
+    private final String splitPattern = "[:-_,/\\\\\\s]+";
+
+
+    private Map<String, Book> bookMap = new HashMap<>();
     private Map<Integer, List<Book>> bookBucketMap = new HashMap<>();
 
     @GetMapping("/search")
@@ -30,6 +35,9 @@ public class SearchController {
         if (query != null) {
             String urlString = "http://localhost:8983/solr/solr-homework";
             SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+
+            bookMap = null;
+            bookBucketMap = null;
 
             List<Book> bookList = new ArrayList<>();
 
@@ -70,7 +78,7 @@ public class SearchController {
     private List<Book> getIdMatchedBooks(SolrClient solr, String query) {
         List<Book> fullMatchedBooks = getFullMatchedBooks(solr, "id", query);
 
-        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "id", query, "%s", 4, 3);
+        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "id", query, "%s", 4, 3, limitForWordsInQuery);
 
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks);
 
@@ -80,7 +88,7 @@ public class SearchController {
     private List<Book> getGenreMatchedBooks(SolrClient solr, String query) {
         List<Book> fullMatchedBooks = getFullMatchedBooks(solr, "genre", query);
 
-        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "genre", query, "%s", 3, 3);
+        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "genre", query, "%s", 3, 3, limitForWordsInQuery);
 
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks);
 
@@ -90,9 +98,9 @@ public class SearchController {
     private List<Book> getAuthorNameMatchedBooks(SolrClient solr, String query) {
         List<Book> fullMatchedBooks = getFullMatchedBooks(solr, "authorName", query);
 
-        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "authorName", query, "%s", 3, 5);
+        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "authorName", query, "%s", 3, 5, limitForWordsInQuery);
 
-        List<Book> partiallyMatchedBooks1 = getPartiallyMatchedBooks(solr, "authorName", query, "%s*", 3, 3);
+        List<Book> partiallyMatchedBooks1 = getPartiallyMatchedBooks(solr, "authorName", query, "%s*", 3, 3, limitForWordsInQuery);
 
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks);
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks1);
@@ -103,8 +111,8 @@ public class SearchController {
     private List<Book> getAuthorSurnameMatchedBooks(SolrClient solr, String query) {
         List<Book> fullMatchedBooks = getFullMatchedBooks(solr, "authorSurname", query);
 
-        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "authorSurname", query, "%s", 3, 5);
-        List<Book> partiallyMatchedBooks1 = getPartiallyMatchedBooks(solr, "authorSurname", query, "%s*", 3, 3);
+        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "authorSurname", query, "%s", 3, 5, limitForWordsInQuery);
+        List<Book> partiallyMatchedBooks1 = getPartiallyMatchedBooks(solr, "authorSurname", query, "%s*", 3, 3, limitForWordsInQuery);
 
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks);
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks1);
@@ -113,9 +121,10 @@ public class SearchController {
     }
 
     private List<Book> getBookNameMatchedBooks(SolrClient solr, String query) {
+
         List<Book> fullMatchedBooks = getFullMatchedBooks(solr, "bookName", query);
 
-        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "bookName", query, "*%s*", 3, 3);
+        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "bookName", query, "*%s*", 3, 3, limitForWordsInQuery);
 
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks);
 
@@ -124,7 +133,7 @@ public class SearchController {
 
     private List<Book> getAnnotationNameMatchedBooks(SolrClient solr, String query) {
 
-        return getPartiallyMatchedBooks(solr, "annotation", query, "*%s*", 3, 1);
+        return getPartiallyMatchedBooks(solr, "annotation", query, "*%s*", 3, 1, 10);
     }
 
     private List<Book> getDateMatchedBooks(SolrClient solr, String query) {
@@ -135,31 +144,33 @@ public class SearchController {
         List<String> queries = Arrays.stream(query.split("[-_,/\\\\\\sw]+"))
                 .collect(Collectors.toList());
 
-        List<Book> partiallyMatchedBooks = queries.stream().map(query1 -> {
-            try {
-                switch (query1.length()) {
-                    case 4:
-                        solrQuery.setQuery(String.format("date:%s", query1));
-                        break;
-                    case 3:
-                        solrQuery.setQuery(String.format("date:%s*", query1));
-                        break;
-                    case 2:
-                        solrQuery.setQuery(String.format("date:*%s", query1));
-                        break;
-                    default:
+        List<Book> partiallyMatchedBooks = queries.stream()
+                .limit(limitForWordsInQuery)
+                .map(query1 -> {
+                    try {
+                        switch (query1.length()) {
+                            case 4:
+                                solrQuery.setQuery(String.format("date:%s", query1));
+                                break;
+                            case 3:
+                                solrQuery.setQuery(String.format("date:%s*", query1));
+                                break;
+                            case 2:
+                                solrQuery.setQuery(String.format("date:*%s", query1));
+                                break;
+                            default:
+                                return new ArrayList<Book>();
+                        }
+
+                        QueryResponse response = solr.query(solrQuery);
+                        List<Book> bookList = response.getBeans(Book.class);
+
+                        return bookList != null ? bookList : new ArrayList<Book>();
+
+                    } catch (SolrServerException | IOException e) {
                         return new ArrayList<Book>();
-                }
-
-                QueryResponse response = solr.query(solrQuery);
-                List<Book> bookList = response.getBeans(Book.class);
-
-                return bookList != null ? bookList : new ArrayList<Book>();
-
-            } catch (SolrServerException | IOException e) {
-                return new ArrayList<Book>();
-            }
-        }).flatMap(List::stream).collect(Collectors.toList());
+                    }
+                }).flatMap(List::stream).collect(Collectors.toList());
 
         partiallyMatchedBooks.forEach(book -> book.addPriority(2));
 
@@ -171,7 +182,7 @@ public class SearchController {
     private List<Book> getLanguageMatchedBooks(SolrClient solr, String query) {
         List<Book> fullMatchedBooks = getFullMatchedBooks(solr, "language", query);
 
-        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "language", query, "%s", 2, 2);
+        List<Book> partiallyMatchedBooks = getPartiallyMatchedBooks(solr, "language", query, "%s", 2, 2, 3);
 
         mergeBooks(fullMatchedBooks, partiallyMatchedBooks);
 
@@ -183,28 +194,30 @@ public class SearchController {
 
         SolrQuery solrQuery = getSolrQuery();
 
-        List<String> queries = Arrays.stream(query.split("[_,/\\\\\\sw]+"))
+        List<String> queries = Arrays.stream(query.split(splitPattern))
                 .collect(Collectors.toList());
 
-        List<Book> partiallyMatchedBooks = queries.stream().map(query1 -> {
-            try {
-                if (Pattern.compile("^([0-9.])+$").matcher(query1).find()) {
+        List<Book> partiallyMatchedBooks = queries.stream()
+                .limit(limitForWordsInQuery)
+                .map(query1 -> {
+                    try {
+                        if (Pattern.compile("^([0-9.])+$").matcher(query1).find()) {
 
-                    while (query1.endsWith("0") && !query1.endsWith(".0")) {
-                        query1 = query1.substring(0, query1.length() - 1);
+                            while (query1.endsWith("0") && !query1.endsWith(".0")) {
+                                query1 = query1.substring(0, query1.length() - 1);
+                            }
+
+                            solrQuery.setQuery(String.format("version:%s", query1));
+
+                            QueryResponse response = solr.query(solrQuery);
+
+                            return response.getBeans(Book.class);
+                        }
+                        return new ArrayList<Book>();
+                    } catch (SolrServerException | IOException e) {
+                        return new ArrayList<Book>();
                     }
-
-                    solrQuery.setQuery(String.format("version:%s", query1));
-
-                    QueryResponse response = solr.query(solrQuery);
-
-                    return response.getBeans(Book.class);
-                }
-                return new ArrayList<Book>();
-            } catch (SolrServerException | IOException e) {
-                return new ArrayList<Book>();
-            }
-        }).flatMap(List::stream).collect(Collectors.toList());
+                }).flatMap(List::stream).collect(Collectors.toList());
 
         partiallyMatchedBooks.forEach(book -> book.addPriority(2));
 
@@ -216,21 +229,22 @@ public class SearchController {
     private List<Book> getFullMatchedBooks(SolrClient solr, String field, String query) {
         try {
             SolrQuery solrQuery = getSolrQuery();
-            solrQuery.setQuery(String.format("%s:%s", field, query));
+            solrQuery.setQuery(String.format("%s:\"%s\"", field, query));
             QueryResponse response = solr.query(solrQuery);
             List<Book> bookList = response.getBeans(Book.class);
-            bookList.forEach(book -> book.addPriority(10));
+            bookList.forEach(book -> book.addPriority(fullMatchedBooksPriority));
             return bookList;
         } catch (IOException | SolrServerException e) {
             return new ArrayList<>();
         }
     }
 
-    private List<Book> getPartiallyMatchedBooks(SolrClient solr, String field, String query, String subQueryPattern, int minSymbolCount, int priority) {
+    private List<Book> getPartiallyMatchedBooks(SolrClient solr, String field, String query, String subQueryPattern, int minSymbolCount, int priority, int limit) {
         SolrQuery solrQuery = getSolrQuery();
 
-        List<String> queries = Arrays.stream(query.split("[-_,/\\\\\\sw]+"))
+        List<String> queries = Arrays.stream(query.toLowerCase().split(splitPattern))
                 .filter(subQuery -> subQuery.length() >= minSymbolCount)
+                .limit(limit)
                 .collect(Collectors.toList());
 
         List<Book> partiallyMatchedBooks = queries.stream().map(subQuery -> {
@@ -266,15 +280,17 @@ public class SearchController {
 
     private SolrQuery getSolrQuery() {
         SolrQuery solrQuery = new SolrQuery();
-        solrQuery.setRows(1000);
+        int rows = 100;
+        solrQuery.setRows(rows);
         return solrQuery;
     }
 
     private void createBooksMaps(List<Book> bookList) {
         bookBucketMap = new HashMap<>();
 
-        for (int i = 0; i < Math.ceil(((double) bookList.size()) / 8); i++) {
-            bookBucketMap.put(i + 1, new ArrayList<>(bookList.subList(i * 8, Math.min(i * 8 + 8, bookList.size()))));
+        int booksOnPage = 8;
+        for (int i = 0; i < Math.ceil(((double) bookList.size()) / booksOnPage); i++) {
+            bookBucketMap.put(i + 1, new ArrayList<>(bookList.subList(i * booksOnPage, Math.min(i * booksOnPage + booksOnPage, bookList.size()))));
         }
 
         bookMap = bookList.stream()
@@ -283,9 +299,15 @@ public class SearchController {
 
     @RequestMapping(value = "/book", method = RequestMethod.POST)
     public String book(@RequestParam("id") String id, @RequestParam("page") int page, Model model) {
-        model.addAttribute("book", bookMap.get(id));
-        model.addAttribute("page", page);
 
-        return "book";
+        if (!bookBucketMap.isEmpty()) {
+
+            model.addAttribute("book", bookMap.get(id));
+            model.addAttribute("page", page);
+
+            return "book";
+        } else {
+            return "search";
+        }
     }
 }
